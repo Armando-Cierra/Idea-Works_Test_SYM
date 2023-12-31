b20aeb945573d9a60759740ccb544d6ec522eaee
Armando-Cierra <armando_cierra@outlook.com>
2023-12-30 20:45:11 -0600

    fixin keyboard event bug on desktop resolution

--- js/general.js
+++ js/general.js
@@ -1,11 +1,10 @@
 //INICIANDO AOS==============================================
-AOS.init();
+AOS.init()
 
 //FECHA DINÁMICA==============================================
 const date = document.getElementById('date')
 const year = new Date().getFullYear()
 date.textContent = `Pampered by IdeaWorks ${year}`
-
 
 //ANIMACIÓN MENÚ===============================================
 //Variables
@@ -18,39 +17,44 @@ let menuState = false
 //eventos
 btnMenu.addEventListener('click', toggleEffect)
 blur.addEventListener('click', toggleEffect)
-links.forEach(link=>{
-    link.addEventListener('click', toggleEffect)
+links.forEach((link) => {
+  link.addEventListener('click', toggleEffect)
 })
-document.addEventListener('keydown', (e)=>{
-    e.key === 'Escape' && toggleEffect()
+document.addEventListener('keydown', (e) => {
+  e.key === 'Escape' && closeMenu()
 })
 
 //funciones
-function toggleEffect(){
-    if(menuState){
-        menuState = false
-        btnMenu.classList.remove('active')
-        mobileMenu.classList.remove('active')
-        blur.style.opacity = '0'
-        setTimeout(()=>{
-            blur.style.display = 'none'
-        }, 250)
+function toggleEffect() {
+  if (menuState) {
+    closeMenu()
+  } else {
+    menuState = true
+    btnMenu.classList.add('active')
+    mobileMenu.classList.add('active')
+    blur.style.display = 'block'
+    setTimeout(() => {
+      blur.style.opacity = '1'
+    }, 10)
 
-        //Habilitar scroll
-        window.onscroll = null;
-        
-    } else {
-        menuState = true
-        btnMenu.classList.add('active')
-        mobileMenu.classList.add('active')
-        blur.style.display = 'block'
-        setTimeout(()=>{
-            blur.style.opacity = '1'
-        }, 10)
-
-        //Bloquear scroll
-        let x = window.scrollX;
-        let y = window.scrollY;
-        window.onscroll = function(){ window.scrollTo(x, y) };
+    //Bloquear scroll
+    let x = window.scrollX
+    let y = window.scrollY
+    window.onscroll = function () {
+      window.scrollTo(x, y)
     }
+  }
+}
+
+function closeMenu() {
+  menuState = false
+  btnMenu.classList.remove('active')
+  mobileMenu.classList.remove('active')
+  blur.style.opacity = '0'
+  setTimeout(() => {
+    blur.style.display = 'none'
+  }, 250)
+
+  //Habilitar scroll
+  window.onscroll = null
 }
